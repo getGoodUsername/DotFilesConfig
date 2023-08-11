dbecb222d8b944a4078aa4b1eba21897a8d4c0a8
Osbaldo Gonzalez Jr <osbaldogonzalezjr@gmail.com>
2023-08-11 01:55:01 -0700

    Made changes to addToHome script to make sure that it will run correctly even if script is called from outside the DotFilesConfig dir

--- addToHome.sh
+++ addToHome.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/bash
 
 # this program creates a symbolic link to a new file in the home directory to the dotfiles in this dir
+# since the script should be in the dir of all the dotfiles
+	# dirname "$0" ensures that from wherever the script is ran, the right
+	# directory is chosen to source the dot files
+allDotfilesDir=$(dirname "$0")
 
 # .[^.] matches a file name that stars with a dot (* won't match with files that start with a dot)
 	# and [^.] specifically matches to a file whose character is NOT a dot
 	# therefore the file name should start with a dot, but the next character MUST NOT be a dot
 	# and that is done to avoid having previous directory ".." show up
-
-allDotFilesDir=$(pwd)
-for dotfile in $allDotFilesDir/.[^.]* 
+for fullDotfileName in $allDotfilesDir/.[^.]* 
 do
-	ln -s $dotfile $HOME/$(basename $dotfile) 
-	echo "Succesfully added $(basename $dotfile) to $HOME!"
+	dotfile=$(basename $fullDotfileName)
+
+	if [[ $dotfile = '.git' ]]; then
+		echo skipped .git
+		continue
+	fi
+
+	ln -s $fullDotfileName $HOME/$dotfile 
+	echo "Succesfully added $dotfile to $HOME!"
 done
