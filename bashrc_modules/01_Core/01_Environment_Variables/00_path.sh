75b004334deffcc8dc9a25e29f1574c5c776b97e
Osbaldo Gonzalez Jr <osbaldogonzalezjr@gmail.com>
2024-10-08 09:16:45 -0700

    updated path env module, only add paths avaliable

--- bashrc_modules/01_Core/01_Environment_Variables/00_path.sh
+++ bashrc_modules/01_Core/01_Environment_Variables/00_path.sh
@@ -1,17 +1,30 @@
 # I WILL BE IGNORING ANY DEFAULTS, AND JUST USING MY PREFERRED DEFAULTS
-export PATH;
-PATH="$(sed -z -e 's/\n/:/g' -e 's/:$//' << EOF
-${HOME}/bin
-${HOME}/.cargo/bin
-${HOME}/go/bin
-/bin
-/usr/bin
-/usr/local/bin
-/sbin
-/usr/sbin
-/usr/local/sbin
-/usr/games
-/usr/local/games
-/snap/bin
-EOF
-)"
+function __Core__Environment_Variables__path__impl
+{
+    local -r IFS=':'
+    local -ar target_path_dirs=(
+        "${HOME}"/bin
+        "${HOME}"/.cargo/bin
+        "${HOME}"/go/bin
+        /bin
+        /usr/bin
+        /usr/local/bin
+        /sbin
+        /usr/sbin
+        /usr/local/sbin
+        /usr/games
+        /usr/local/games
+        /snap/bin
+    )
+    local -a available_path_dirs;
+
+    for dir in "${target_path_dirs[@]}"; do
+        if [[ -d "${dir}" && -x "${dir}" ]]; then available_path_dirs+=("${dir}"); fi
+    done
+
+    export PATH;
+    PATH="${available_path_dirs[*]}";
+}
+
+__Core__Environment_Variables__path__impl
+unset -f __Core__Environment_Variables__path__impl
